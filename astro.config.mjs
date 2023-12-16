97bd6e538a2005940ddf4e9ca5e5be029d2047ae
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2023-12-16 10:08:31 +0530

    Vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import storyblokIntegration from "@storyblok/astro";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [
+    react(),
+    tailwind(),
+    storyblokIntegration({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: "",
+      apiOptions: {},
+    }),
+  ],
 });
