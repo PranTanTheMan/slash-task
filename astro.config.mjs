7ea11ead49eadebdc23a48c680c95d58a95b5bfb
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2023-12-19 08:34:45 +0530

    Configured Storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,34 @@
 import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
-import storyblokIntegration from "@storyblok/astro";
-
+// import basicSsl from "@vitejs/plugin-basic-ssl";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    react(),
-    tailwind(),
-    storyblokIntegration({
+    storyblok({
+      //accessToken: env.STORYBLOK_TOKEN,
       accessToken: env.STORYBLOK_TOKEN,
-      components: "",
-      apiOptions: {},
+      apiOptions: {
+        region: "",
+      },
+      bridge: {
+        customParent: "https://app.storyblok.com",
+      },
+      components: {
+        page: "storyblok/Page",
+        navbar: "storyblok/Navbar",
+        links: "storyblok/Links",
+      },
     }),
+    tailwind(),
   ],
+  // vite: {
+  //   plugins: [basicSsl()],
+  //   server: {
+  //     https: true,
+  //   },
+  // },
 });
